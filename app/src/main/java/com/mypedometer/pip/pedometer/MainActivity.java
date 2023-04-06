d7295f9933827733298e920af35c8a33e539500a
CatalinButacu <ionel-catalin.butacu@student.tuiasi.ro>
2023-04-06 18:53:54 +0300

    [UI] text action update

--- app/src/main/java/com/mypedometer/pip/pedometer/MainActivity.java
+++ app/src/main/java/com/mypedometer/pip/pedometer/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Ai apasat butonul. Felicitari!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
