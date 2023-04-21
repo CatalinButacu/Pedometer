9c1f795d1a1b1643dfa1e479cf2535a98b9000fe
CatalinButacu <ionel-catalin.butacu@student.tuiasi.ro>
2023-04-21 19:44:46 +0300

    DEV-18 Implementarea sistemului de notificari in aplicatie -- Partea1: SUCCES LOGIN

--- app/src/main/java/com/mypedometer/pip/pedometer/MainActivity.java
+++ app/src/main/java/com/mypedometer/pip/pedometer/MainActivity.java
@@ -1,12 +1,17 @@
 package com.mypedometer.pip.pedometer;
 
 import android.app.FragmentTransaction;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +19,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -42,6 +48,22 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
+
+        //View LoginView = inflater.inflate(R.layout.login_layout, container, false);
+        Button buttonLogin = (Button)findViewById(R.id.login_button);
+
+        try {
+            //creare canal notifcare
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel("Login Notification", "Login Notification", NotificationManager.IMPORTANCE_DEFAULT);
+                NotificationManager manager = getSystemService(NotificationManager.class);
+
+                if (manager != null)
+                    manager.createNotificationChannel(channel);
+            }
+        } catch (Exception e) {
+            System.out.println("CANALUL NU A PUTUT FI CREAT");
+        }
 
     }
 }
--- app/src/main/java/com/mypedometer/pip/pedometer/ui/main/LoginFragment.java
+++ app/src/main/java/com/mypedometer/pip/pedometer/ui/main/LoginFragment.java
@@ -3,6 +3,8 @@ package com.mypedometer.pip.pedometer.ui.main;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -11,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.mypedometer.pip.pedometer.MainActivity;
 import com.mypedometer.pip.pedometer.R;
 
 public class LoginFragment extends Fragment {
@@ -24,8 +27,39 @@ public class LoginFragment extends Fragment {
 
         Button loginButton = (Button)LoginView.findViewById(R.id.login_button);
         //de implementat actionListener la butoanele de login si sing up
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean valid = true;
+
+                //TODO:: DE IMPLEMENTAT VALIDAREA CU BAZA DE DATE
+
+                if (valid) {
+                    //TODO:: DE INITIALIZAT NOUA FEREASTRA CU PROFILUL
+
+                    //TRIMITERE NOTIFICARE SYSTEM LOGARE
+                    try {
+                        SentNotificatrion();
+                    } catch (Exception e) {
+                        System.out.println("Ascultatorul pe butonul de login nu a putut fi stabilit!");
+                    }
+                }
+            }
+        });
         return LoginView;
     }
 
+    void SentNotificatrion() {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getActivity(), "Login Notification");
+        mBuilder.setContentTitle(getResources().getString(R.string.app_name));
+        mBuilder.setContentText(getResources().getString(R.string.notification_login));
+        mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        mBuilder.setAutoCancel(true);
+
+        NotificationManagerCompat managerNotification = NotificationManagerCompat.from(getActivity());
+        managerNotification.notify(0, mBuilder.build());
+    }
 
 }
