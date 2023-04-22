19ee3e06572035027fefa6173ed6ef0945ff17aa
CatalinButacu <ionel-catalin.butacu@student.tuiasi.ro>
2023-04-22 14:41:32 +0300

    DEV-17 Implementarea bazei de date in aplicatie -- UNSTABLE

---
+++ app/src/main/java/com/mypedometer/pip/pedometer/ConnectionDB.java
@@ -0,0 +1,95 @@
+package com.mypedometer.pip.pedometer;
+
+import android.content.Context;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionDB {
+
+    //database connection
+    private static final String host = "europe-central2.864cfdd1-12ac-4ced-b28d-ae03cbeb2101.gcp.ybdb.io";
+    private static final String port = "5433";
+    private static final String database = "postgres";
+    private static final String user = "admin";
+    private static final String password = "kS5lkG416PfR1I9K-z0AfjzbreHjZn";
+    private static final String url = "jdbc:yugabytedb:/" + host + ":" + port + "/" + database + "?user=" + user + "&password=" + password;
+
+    private static final String urlShield = "./ysqlsh \"host=europe-central2.864cfdd1-12ac-4ced-b28d-ae03cbeb2101.gcp.ybdb.io \\\n" +
+            "user=Users\\" +
+            "dbname=postgres\\" +
+            "sslmode=verify-full\\" +
+            "sslrootcert=./root.crt";
+
+
+
+
+    //database communication
+    //private Context m_Context;
+    private Connection m_Connection;
+    private Statement m_Statement;
+    private ResultSet m_ResultSet;
+
+    public ConnectionDB() {
+        //this.m_Context = context;
+    }
+
+    public Connection getConnection() {
+        try {
+            System.out.println("Loading PostgreSQL JDBC driver...");
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            System.out.println("Connecting to database...");
+            //m_Connection = DriverManager.getConnection(url, user, password);
+            //m_Connection = DriverManager.getConnection(url);// getConnection(urlShield, user, password);
+            m_Connection = DriverManager.getConnection("jdbc:yugabytedb://"+host+":"+port+"/"+database+"?user="+user+"&password="+password);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Failed to connect to database: " + e.getMessage());
+        }
+        return m_Connection;
+    }
+
+
+    public Statement getStatement() {
+        try {
+            m_Statement = m_Connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return m_Statement;
+    }
+
+    public ResultSet getResultSet(String sql) {
+        try {
+            m_ResultSet = m_Statement.executeQuery(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return m_ResultSet;
+    }
+
+    public void close() {
+        try {
+            if (m_ResultSet != null) {
+                m_ResultSet.close();
+            }
+            if (m_Statement != null) {
+                m_Statement.close();
+            }
+            if (m_Connection != null) {
+                m_Connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        m_ResultSet = null;
+    }
+}
--- app/src/main/java/com/mypedometer/pip/pedometer/ui/main/LoginFragment.java
+++ app/src/main/java/com/mypedometer/pip/pedometer/ui/main/LoginFragment.java
@@ -1,20 +1,22 @@
 package com.mypedometer.pip.pedometer.ui.main;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
-import com.mypedometer.pip.pedometer.MainActivity;
+import com.mypedometer.pip.pedometer.ConnectionDB;
 import com.mypedometer.pip.pedometer.R;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class LoginFragment extends Fragment {
     public LoginFragment() {
@@ -25,36 +27,76 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View LoginView = inflater.inflate(R.layout.login_layout, container, false);
 
-        Button loginButton = (Button)LoginView.findViewById(R.id.login_button);
-        //de implementat actionListener la butoanele de login si sing up
+        Button loginButton = (Button) LoginView.findViewById(R.id.login_button);
+        Button signupButton = (Button) LoginView.findViewById(R.id.signup_button);
 
+        EditText emailEditText = (EditText) LoginView.findViewById(R.id.email_input);
+        EditText passwordEditText = (EditText) LoginView.findViewById(R.id.password_input);
+
+        //TODO:: de implementat actionListener la login si sing up care sa duca la celelalte pagini
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 boolean valid = true;
+                String email = "";
+                String password = "";
+
 
                 //TODO:: DE IMPLEMENTAT VALIDAREA CU BAZA DE DATE
+                try {
+                    email = emailEditText.getText().toString();
+                    password = passwordEditText.getText().toString();
+
+                    if (email.isEmpty() || password.isEmpty()) {
+                        valid = false;
+                    }
+                } catch (Exception e) {
+                    valid = false;
+                }
+
 
                 if (valid) {
-                    //TODO:: DE INITIALIZAT NOUA FEREASTRA CU PROFILUL
-
-                    //TRIMITERE NOTIFICARE SYSTEM LOGARE
                     try {
-                        SentNotificatrion();
+                        ConnectionDB db = new ConnectionDB();
+                        String query = "SELECT Email, Password FROM Users WHERE Email = ? AND Password = ?";
+                        PreparedStatement statement = db.getConnection().prepareStatement(query);
+                        statement.setString(1, email); // Set the value of the first placeholder to the username
+                        statement.setString(2, password); // Set the value of the second placeholder to the password
+                        ResultSet resultSet = statement.executeQuery();
+
+                        if (resultSet.next()) {
+                            // Credentials are valid, proceed with login
+                        } else {
+                            // Credentials are invalid, show an error message
+                        }
+
+                        statement.close();
+                        resultSet.close();
+                        db.close();
                     } catch (Exception e) {
-                        System.out.println("Ascultatorul pe butonul de login nu a putut fi stabilit!");
+                        valid = false;
                     }
+                }
+
+
+                //TODO:: DE INITIALIZAT NOUA FEREASTRA CU PROFILUL
+
+                //TRIMITERE NOTIFICARE SYSTEM LOGARE
+                try {
+                    SentNotificatrion(valid);
+                } catch (Exception e) {
+                    System.out.println("Ascultatorul pe butonul de login nu a putut fi stabilit!");
                 }
             }
         });
         return LoginView;
     }
 
-    void SentNotificatrion() {
+    void SentNotificatrion(boolean success) {
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getActivity(), "Login Notification");
         mBuilder.setContentTitle(getResources().getString(R.string.app_name));
-        mBuilder.setContentText(getResources().getString(R.string.notification_login));
+        mBuilder.setContentText(getResources().getString(success ? R.string.notification_login_success : R.string.notification_login_failure));
         mBuilder.setSmallIcon(R.mipmap.ic_launcher);
         mBuilder.setAutoCancel(true);
 
