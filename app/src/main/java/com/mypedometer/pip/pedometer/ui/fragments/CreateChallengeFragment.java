7f43667286c0cf6bde63fd543a3f7fed724dc8cc
Mihai <mihaialex989@gmail.com>
2023-05-04 14:36:41 +0300

    Creare fragment pt CREATECHALLENGEFRAGMENT

---
+++ app/src/main/java/com/mypedometer/pip/pedometer/ui/fragments/CreateChallengeFragment.java
@@ -0,0 +1,24 @@
+package com.mypedometer.pip.pedometer.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mypedometer.pip.pedometer.R;
+
+public class CreateChallengeFragment extends Fragment {
+
+    public CreateChallengeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View createChallengeView = inflater.inflate(R.layout.create_challenge_layout, container, false);
+        return createChallengeView;
+    }
+}
