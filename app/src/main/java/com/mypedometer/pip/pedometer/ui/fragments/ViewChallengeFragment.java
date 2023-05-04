8d8b2f5d92fd0f4ec35302bb1dbbf12573a7274d
DESKTOP-J1SKMTG\Cosmin <cosmin.chiruta@student.tuiasi.ro>
2023-05-04 14:45:28 +0300

    Added Fragment for view_challenge_layout.

---
+++ app/src/main/java/com/mypedometer/pip/pedometer/ui/fragments/ViewChallengeFragment.java
@@ -0,0 +1,26 @@
+package com.mypedometer.pip.pedometer.ui.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.mypedometer.pip.pedometer.R;
+
+public class ViewChallengeFragment extends Fragment {
+
+    public ViewChallengeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.view_challenge_layout, container, false);
+        // TODO: initialize views and data
+        return view;
+    }
+}
